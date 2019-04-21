ed36aeff00750295ff3fd242571f0d04b9e918bd
Felix Leber <email@felixleber.de>
2019-04-21 13:17:54 +0200

    Add empty services and controllers Update build.sbt

--- app/services/UserService.scala
+++ app/services/UserService.scala
@@ -1,5 +1,33 @@
 package services
 
-class UserService {
+import javax.inject._
+import models.User
+
+trait UserService{
+  def getUser(id: String): User
+  def addUser(id: String, user: User)
+  def userExists(id:String): Boolean
+}
+
+@Singleton
+class Users extends UserService {
+  private var users: Map[String, User] = Map()
+
+  override def addUser(id: String, user: User){
+    users += (id -> user)
+  }
+
+  @throws(classOf[Exception])
+  override def getUser(id: String): User ={
+    if (userExists(id)){
+      val found: User = users(id)
+      return found
+    }
+    throw new Exception("User not found")
+  }
+
+  override def userExists(id: String): Boolean ={
+    users contains(id)
+  }
 
 }
